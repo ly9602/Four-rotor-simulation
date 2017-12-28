64866c48dabddc396690ae4497b42a0c5d41443b
墨基 <liya9602@qq.com>
2017-12-28 15:25:09 +0800

    OrbitCamera
    Camera

---
+++ OrbitCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitCamera : MonoBehaviour
+{
+    [SerializeField] private Transform sxy;
+    public float rotSpeed = 1.5f;
+    private float _rotY;
+    private Vector3 _offset;
+  
+	void Start () {
+        _rotY = transform.eulerAngles.y;
+        _offset = sxy.position - transform.position;
+	}
+	
+
+	void Update () {
+		float horInput = Input.GetAxis("Horizontal");
+		if (horInput != 0)
+		{
+			_rotY += horInput * rotSpeed;
+		}
+		else
+		{
+			_rotY += Input.GetAxis("Mouse X") * rotSpeed * 3;
+		}
+        Quaternion rotation = Quaternion.Euler(0, _rotY, 0);
+        transform.position = sxy.position - (rotation * _offset);
+        transform.LookAt(sxy); 
+	}
+}
